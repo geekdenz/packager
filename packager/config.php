6d887a22e5fd154fa9c0074c0fbc7e735431c107
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2014-01-22 16:58:24 +1300

    repository corrected

--- packager/config.php
+++ packager/config.php
@@ -16,7 +16,7 @@ return array(
         //'user' => 'heuert', // optional. The user to upload the deb package, `whoami` by default
         //'repository' => 'repository.test:/var/www/dists/stable/main/binary-amd64', // ssh url of repository to put this in
         //'repository' => 'uvm:/tmp/',
-        'repository' => 'repository.test.zen.landcareresearch.co.nz:/var/www/dists/precise/main/binary-amd64',
+        'repository' => 'heuert@repository.test.zen.landcareresearch.co.nz:/var/www/dists/precise/main/binary-amd64',
         'url' => 'https://github.com/geekdenz/packager',
         'vendor' => 'Landcare Research',
 
